4ff7fbdc9142cf9d9355eff077b532658419515d
cdefaz08 <cdefaz08@gmail.com>
2025-03-19 17:46:03 -0400

    Users Table update

--- Layout/MainWindow.py
+++ Layout/MainWindow.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, uic
 import sys
 from item_search import ItemSearchWindow  # Import the new sub-window
+from User_table import UsersTableWindow
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -10,10 +11,12 @@ class MainWindow(QtWidgets.QMainWindow):
         # Reference Actions
         self.actionLogout = self.findChild(QtWidgets.QAction, 'actionLogout')
         self.actionItem_Search = self.findChild(QtWidgets.QAction, 'actionItem_Search')
+        self.actionUser_table = self.findChild(QtWidgets.QAction, 'actionUsers')
 
         # Connect Actions
         self.actionLogout.triggered.connect(self.logout)
         self.actionItem_Search.triggered.connect(self.open_item_search)
+        self.actionUser_table.triggered.connect(self.open_user_table)
 
     def logout(self):
         confirm = QtWidgets.QMessageBox.question(
@@ -28,6 +31,11 @@ class MainWindow(QtWidgets.QMainWindow):
     def open_item_search(self):
         self.item_search_window = ItemSearchWindow()
         self.item_search_window.show()
+
+    def open_user_table(self):
+        self.Users_window = UsersTableWindow()
+        self.Users_window.show()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
